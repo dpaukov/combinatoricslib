731d9a2167bdd14f66090ae7a01abfa7f9b62df9
d.paukov <d.paukov@7f51f61c-e9a7-11de-8dea-4d08a2fd72f5>
2010-08-02 14:51:53 +0000

    Determinant has been added to the Util class + jUnit tests

--- trunk/src/combinatorics/util/Util.java
+++ trunk/src/combinatorics/util/Util.java
@@ -2,6 +2,7 @@ package combinatorics.util;
 
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.math.BigDecimal;
 
 /**
  * Utility class for combinatorial package
@@ -241,5 +242,120 @@ public class Util {
 	    return new Point2D.Double(x, y);
 	}
 	return null;
+    }
+
+    /**
+     * Calculates determinant of the matrix a
+     */
+    public static double det(double[][] a, final double eps) {
+	int n = a.length;
+	double res = 1;
+	boolean[] used = new boolean[n];
+	for (int i = 0; i < n; i++) {
+	    int p;
+	    for (p = 0; p < n; p++)
+		if (!used[p] && Math.abs(a[p][i]) > eps)
+		    break;
+	    if (p >= n)
+		return 0;
+	    res *= a[p][i];
+	    used[p] = true;
+	    double z = 1 / a[p][i];
+	    for (int j = 0; j < n; j++)
+		a[p][j] *= z;
+	    for (int j = 0; j < n; ++j)
+		if (j != p) {
+		    z = a[j][i];
+		    for (int k = 0; k < n; ++k)
+			a[j][k] -= z * a[p][k];
+		}
+	}
+	return res;
+    }
+
+    /**
+     * Calculates the determinant of the matrix a
+     */
+    public static BigDecimal detCrout(BigDecimal a[][], int n) {
+	try {
+	    for (int i = 0; i < n; i++) {
+		boolean nonzero = false;
+		for (int j = 0; j < n; j++)
+		    if (a[i][j].compareTo(new BigDecimal(0)) > 0)
+			nonzero = true;
+		if (!nonzero)
+		    return BigDecimal.ZERO;
+	    }
+
+	    BigDecimal scaling[] = new BigDecimal[n];
+	    for (int i = 0; i < n; i++) {
+		BigDecimal big = new BigDecimal(0);
+		for (int j = 0; j < n; j++)
+		    if (a[i][j].abs().compareTo(big) > 0)
+			big = a[i][j].abs();
+		scaling[i] = (new BigDecimal(1)).divide(big, 100,
+			BigDecimal.ROUND_HALF_EVEN);
+	    }
+
+	    int sign = 1;
+
+	    for (int j = 0; j < n; j++) {
+
+		for (int i = 0; i < j; i++) {
+		    BigDecimal sum = a[i][j];
+		    for (int k = 0; k < i; k++)
+			sum = sum.subtract(a[i][k].multiply(a[k][j]));
+		    a[i][j] = sum;
+		}
+
+		BigDecimal big = new BigDecimal(0);
+		int imax = -1;
+		for (int i = j; i < n; i++) {
+		    BigDecimal sum = a[i][j];
+		    for (int k = 0; k < j; k++)
+			sum = sum.subtract(a[i][k].multiply(a[k][j]));
+		    a[i][j] = sum;
+		    BigDecimal cur = sum.abs();
+		    cur = cur.multiply(scaling[i]);
+		    if (cur.compareTo(big) >= 0) {
+			big = cur;
+			imax = i;
+		    }
+		}
+
+		if (j != imax) {
+
+		    for (int k = 0; k < n; k++) {
+			BigDecimal t = a[j][k];
+			a[j][k] = a[imax][k];
+			a[imax][k] = t;
+		    }
+
+		    BigDecimal t = scaling[imax];
+		    scaling[imax] = scaling[j];
+		    scaling[j] = t;
+
+		    sign = -sign;
+		}
+
+		if (j != n - 1)
+		    for (int i = j + 1; i < n; i++)
+			a[i][j] = a[i][j].divide(a[j][j], 100,
+				BigDecimal.ROUND_HALF_EVEN);
+
+	    }
+
+	    BigDecimal result = new BigDecimal(1);
+	    if (sign == -1)
+		result = result.negate();
+	    for (int i = 0; i < n; i++)
+		result = result.multiply(a[i][i]);
+
+	    return result.divide(BigDecimal.valueOf(1), 0,
+		    BigDecimal.ROUND_HALF_EVEN);
+
+	} catch (Exception e) {
+	    return BigDecimal.ZERO;
+	}
     }
 }
