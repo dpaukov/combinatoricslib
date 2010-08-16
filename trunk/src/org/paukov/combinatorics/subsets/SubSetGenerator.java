a7fa7e0c3bcea7b1750ddd0a5180f504a1d0e3b6
d.paukov <d.paukov@7f51f61c-e9a7-11de-8dea-4d08a2fd72f5>
2010-08-16 15:26:59 +0000

    javadoc comments were added for SubSet generator/iterator

--- trunk/src/org/paukov/combinatorics/subsets/SubSetGenerator.java
+++ trunk/src/org/paukov/combinatorics/subsets/SubSetGenerator.java
@@ -5,24 +5,48 @@ import org.paukov.combinatorics.Generator;
 import org.paukov.combinatorics.Iterator;
 import org.paukov.combinatorics.util.Util;
 
+/**
+ * This generator generates all subsets of the specified set (core set)
+ * 
+ * @param <T>
+ *            Type of elements in the set
+ */
 public class SubSetGenerator<T> extends Generator<T> {
 
-	protected final CombinatoricsVector<T> _coreSet;
+    /**
+     * Core set
+     */
+    protected final CombinatoricsVector<T> _coreSet;
 
-	public SubSetGenerator(CombinatoricsVector<T> coreSet) {
-		_coreSet = new CombinatoricsVector<T>(coreSet);
-	}
+    /**
+     * Constructor
+     * 
+     * @param coreSet
+     *            Core set
+     */
+    public SubSetGenerator(CombinatoricsVector<T> coreSet) {
+	_coreSet = new CombinatoricsVector<T>(coreSet);
+    }
 
-	public CombinatoricsVector<T> getCoreObject() {
-		return _coreSet;
-	}
+    /**
+     * Returns the core set
+     */
+    public CombinatoricsVector<T> getCoreObject() {
+	return _coreSet;
+    }
 
-	public long getNumberOfGeneratedObjects() {
-		return Util.pow2(_coreSet.getSize());
-	}
+    /**
+     * Returns the number of the subsets
+     */
+    public long getNumberOfGeneratedObjects() {
+	return Util.pow2(_coreSet.getSize());
+    }
 
-	public Iterator<CombinatoricsVector<T>> createIterator() {
-		return new SubSetIterator<T>(this);
-	}
+    /**
+     * Creates the iterator over the all subsets
+     */
+    public Iterator<CombinatoricsVector<T>> createIterator() {
+	return new SubSetIterator<T>(this);
+    }
 
 }
