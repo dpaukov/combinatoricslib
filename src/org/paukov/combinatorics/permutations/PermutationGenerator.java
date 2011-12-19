2b46897a5eee5375f79376cce07ef9b6662854e3
dpaukov <d.paukov@gmail.com>
2011-12-19 15:34:26 +0000

    More javadoc comments were added for the permutations without repetitions

--- src/org/paukov/combinatorics/permutations/PermutationGenerator.java
+++ src/org/paukov/combinatorics/permutations/PermutationGenerator.java
@@ -7,7 +7,65 @@ import org.paukov.combinatorics.util.Util;
 
 /**
  * This generator generates all permutations of specified core vector
+ * <p>
+ * A permutation is an ordering of a set in the context of all possible
+ * orderings. For example, the set containing the first three digits, 123, has
+ * six permutations: 123, 132, 213, 231, 312, and 321.
+ * <p>
+ * This is a example of permutation of 3 string items:
+ * <p>
+ * <blockquote>
  * 
+ * <pre>
+ * // create array of initial items
+ * ArrayList&lt;String&gt; array = new ArrayList&lt;String&gt;();
+ * array.add(&quot;one&quot;);
+ * array.add(&quot;two&quot;);
+ * array.add(&quot;three&quot;);
+ * 
+ * // create combinatorics vector
+ * CombinatoricsVector&lt;String&gt; initialVector = new CombinatoricsVector&lt;String&gt;(
+ * 		array);
+ * 
+ * // create permutation generator
+ * Generator&lt;String&gt; generator = new PermutationGenerator&lt;String&gt;(initialVector);
+ * 
+ * // print the number of generated permutations
+ * System.out.println(&quot;Number of permutations is: &quot;
+ * 		+ generator.getNumberOfGeneratedObjects());
+ * 
+ * // create iterator
+ * Iterator&lt;CombinatoricsVector&lt;String&gt;&gt; itr = generator.createIterator();
+ * 
+ * // go through the iterator
+ * while (itr.hasNext()) {
+ * 	CombinatoricsVector&lt;String&gt; permutation = itr.next();
+ * 	System.out.println(permutation);
+ * }
+ * </pre>
+ * 
+ * </blockquote>
+ * <p>
+ * And the result
+ * <p>
+ * <blockquote>
+ * 
+ * <pre>
+ *    Number of permutations is: 6
+ *    CombinatoricsVector=[[one, two, three]], size=3]
+ *    CombinatoricsVector=[[one, three, two]], size=3]
+ *    CombinatoricsVector=[[three, one, two]], size=3]
+ *    CombinatoricsVector=[[three, two, one]], size=3]
+ *    CombinatoricsVector=[[two, three, one]], size=3]
+ *    CombinatoricsVector=[[two, one, three]], size=3]
+ * </pre>
+ * 
+ * </blockquote>
+ * <p>
+ * 
+ * @author Dmytro.Paukov
+ * @see CombinatoricsVector
+ * @see PermutationIterator
  * @param <T>
  *            Type of elements in permutation
  */
