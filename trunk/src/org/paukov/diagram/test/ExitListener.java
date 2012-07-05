3868ec09eb67f0ccb543775b2632c67f457234ed
d.paukov@gmail.com <d.paukov@gmail.com@7f51f61c-e9a7-11de-8dea-4d08a2fd72f5>
2012-07-05 09:06:39 +0000

    All stuff unrelated to combinatorics were removed

--- trunk/src/org/paukov/diagram/test/ExitListener.java
+++
@@ -1,9 +0,0 @@
-package org.paukov.diagram.test;
-
-import java.awt.event.*;
-
-public class ExitListener extends WindowAdapter {
-    public void windowClosing(WindowEvent event) {
-	System.exit(0);
-    }
-}
