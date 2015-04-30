0d82260b9cc8507418e99d8e7c9d3bee9264a40a
dpaukov <d.paukov@gmail.com>
2015-04-29 17:00:02 -0700

    Refactor the examples in the main method

--- src/main/java/org/paukov/examples/Main.java
+++ src/main/java/org/paukov/examples/Main.java
@@ -21,298 +21,308 @@ import org.paukov.combinatorics.util.ComplexCombinationGenerator;
  */
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
 
-		/**
-		 * Examples
-		 */
-		permutationWithoutRepetitions();
-		permutationWithRepetitions();
+        /**
+         * Permutations
+         */
+        permutation_without_repetitions();
+        permutation_with_repetitions();
 
-		simpleCombinations();
-		multiCombinations();
+        /**
+         * Combinations
+         */
+        simple_combinations();
+        multi_combinations();
 
-		simpleSubSets();
-		duplicateSubSets();
+        /**
+         * Subsets
+         */
+        simple_subsets();
+        duplicate_subsets();
 
-		integerPartition();
+        /**
+         * Integer partitions and compositions
+         */
+        integer_partition();
+        integer_composition();
 
-		integerComposition();
+        /**
+         * Complex combination (List partitions)
+         */
+        complex_combination_indexes_example();
+        complex_combination_example();
 
-		complexCombinationIndexesExample();
-		complexCombinationExample();
+        simple_with_equal_elements_permutation();
 
-		simpleWithEqualElementsPermutation();
+        /**
+         * Examples of the integer vectors
+         */
+        simple_five_integer_composition_range();
 
-		/**
-		 * Examples of the integer vectors
-		 */
-		simpleFiveIntegerCompositionRange();
+        /**
+         * Filters
+         */
+        filtered_combinations();
 
-		/**
-		 * Filters
-		 */
-		filteredCombinations();
+    }
 
-	}
+    static void simple_combinations() {
 
-	static void simpleCombinations() {
+        System.out.println("===== Simple Combinations: =====");
 
-		System.out.println("simpleCombinations");
+        // Create the initial vector
+        ICombinatoricsVector<String> initialVector = Factory
+                .createVector(new String[]{"red", "black", "white", "green",
+                        "blue"});
 
-		// Create the initial vector
-		ICombinatoricsVector<String> initialVector = Factory
-				.createVector(new String[] { "red", "black", "white", "green",
-						"blue" });
+        // Create a simple combination generator to generate 3-combinations of
+        // the initial vector
+        Generator<String> gen = Factory.createSimpleCombinationGenerator(
+                initialVector, 3);
 
-		// Create a simple combination generator to generate 3-combinations of
-		// the initial vector
-		Generator<String> gen = Factory.createSimpleCombinationGenerator(
-				initialVector, 3);
+        // Print all possible combinations
+        for (ICombinatoricsVector<String> combination : gen) {
+            System.out.println(combination);
+        }
 
-		// Print all possible combinations
-		for (ICombinatoricsVector<String> combination : gen) {
-			System.out.println(combination);
-		}
+    }
 
-	}
+    static void multi_combinations() {
 
-	static void multiCombinations() {
+        System.out.println("===== Multi Combinations: =====");
 
-		System.out.println("multiCombinations");
+        // Create the initial vector of (apple, orange)
+        ICombinatoricsVector<String> initialVector = Factory
+                .createVector(new String[]{"apple", "orange"});
 
-		// Create the initial vector of (apple, orange)
-		ICombinatoricsVector<String> initialVector = Factory
-				.createVector(new String[] { "apple", "orange" });
+        // Create a multi-combination generator to generate 3-combinations of
+        // the initial vector
+        Generator<String> gen = Factory.createMultiCombinationGenerator(
+                initialVector, 3);
 
-		// Create a multi-combination generator to generate 3-combinations of
-		// the initial vector
-		Generator<String> gen = Factory.createMultiCombinationGenerator(
-				initialVector, 3);
+        // Print all possible combinations
+        for (ICombinatoricsVector<String> combination : gen) {
+            System.out.println(combination);
+        }
 
-		// Print all possible combinations
-		for (ICombinatoricsVector<String> combination : gen) {
-			System.out.println(combination);
-		}
+    }
 
-	}
+    static void permutation_with_repetitions() {
 
-	static void permutationWithRepetitions() {
+        System.out.println("===== Permutation With Repetitions: =====");
 
-		System.out.println("permutationWithRepetitions");
+        // Create the initial set/vector of 2 elements (apple, orange)
+        ICombinatoricsVector<String> originalVector = Factory
+                .createVector(new String[]{"apple", "orange"});
 
-		// Create the initial set/vector of 2 elements (apple, orange)
-		ICombinatoricsVector<String> originalVector = Factory
-				.createVector(new String[] { "apple", "orange" });
+        // Create the generator by calling the appropriate method in the Factory
+        // class
+        Generator<String> gen = Factory
+                .createPermutationWithRepetitionGenerator(originalVector, 3);
 
-		// Create the generator by calling the appropriate method in the Factory
-		// class
-		Generator<String> gen = Factory
-				.createPermutationWithRepetitionGenerator(originalVector, 3);
+        // Print the result
+        for (ICombinatoricsVector<String> perm : gen)
+            System.out.println(perm);
 
-		// Print the result
-		for (ICombinatoricsVector<String> perm : gen)
-			System.out.println(perm);
+    }
 
-	}
+    static void permutation_without_repetitions() {
 
-	static void permutationWithoutRepetitions() {
+        System.out.println("===== Permutations Without Repetitions: =====");
 
-		System.out.println("permutationWithoutRepetitions");
+        // Create the initial set/vector of 3 elements (apple, orange, cherry)
+        ICombinatoricsVector<String> originalVector = Factory
+                .createVector(new String[]{"apple", "orange", "cherry"});
 
-		// Create the initial set/vector of 3 elements (apple, orange, cherry)
-		ICombinatoricsVector<String> originalVector = Factory
-				.createVector(new String[] { "apple", "orange", "cherry" });
+        // Create the permutation generator by calling the appropriate method in
+        // the Factory class
+        Generator<String> gen = Factory
+                .createPermutationGenerator(originalVector);
 
-		// Create the permutation generator by calling the appropriate method in
-		// the Factory class
-		Generator<String> gen = Factory
-				.createPermutationGenerator(originalVector);
+        // Print the result
+        for (ICombinatoricsVector<String> perm : gen)
+            System.out.println(perm);
 
-		// Print the result
-		for (ICombinatoricsVector<String> perm : gen)
-			System.out.println(perm);
+    }
 
-	}
+    static void complex_combination_indexes_example() {
 
-	static void complexCombinationIndexesExample() {
+        System.out.println("===== Complex Combination Indexes Example (List partitions): =====");
 
-		System.out.println("complexCombinationIndexesExample");
+        // A list of elements
+        String[] elements = new String[]{"A", "B", "B", "C"};
 
-		// A list of elements
-		String[] elements = new String[] { "A", "B", "B", "C" };
+        // create a combinatorics vector of indexes (1, 1, 3)
+        ICombinatoricsVector<Integer> indixesVector = Factory
+                .createVector(new Integer[]{1, 1, 3});
 
-		// create a combinatorics vector of indexes (1, 1, 3)
-		ICombinatoricsVector<Integer> indixesVector = Factory
-				.createVector(new Integer[] { 1, 1, 3 });
+        // Create a complex-combination generator
+        Generator<ICombinatoricsVector<Integer>> complexGenerator = new ComplexCombinationGenerator<Integer>(
+                indixesVector, 2);
 
-		// Create a complex-combination generator
-		Generator<ICombinatoricsVector<Integer>> complexGenerator = new ComplexCombinationGenerator<Integer>(
-				indixesVector, 2);
+        // Iterate the elements
+        for (ICombinatoricsVector<ICombinatoricsVector<Integer>> combination : complexGenerator) {
 
-		// Iterate the elements
-		for (ICombinatoricsVector<ICombinatoricsVector<Integer>> combination : complexGenerator) {
+            String str = ComplexCombinationGenerator.convertIndexes2String(
+                    elements, combination);
 
-			String str = ComplexCombinationGenerator.convertIndexes2String(
-					elements, combination);
+            System.out.println(str);
+        }
+    }
 
-			System.out.println(str);
-		}
-	}
+    static void complex_combination_example() {
 
-	static void complexCombinationExample() {
+        System.out.println("complexCombinationExample");
+        System.out.println("===== Complex Combination Example: =====");
 
-		System.out.println("complexCombinationExample");
+        // create a combinatorics vector (A, B, B, C)
+        ICombinatoricsVector<String> vector = Factory
+                .createVector(new String[]{"A", "B", "B", "C"});
 
-		// create a combinatorics vector (A, B, B, C)
-		ICombinatoricsVector<String> vector = Factory
-				.createVector(new String[] { "A", "B", "B", "C" });
+        // Create a complex-combination generator
+        Generator<ICombinatoricsVector<String>> gen = new ComplexCombinationGenerator<String>(
+                vector, 2);
 
-		// Create a complex-combination generator
-		Generator<ICombinatoricsVector<String>> gen = new ComplexCombinationGenerator<String>(
-				vector, 2);
+        // Iterate the combinations
+        for (ICombinatoricsVector<ICombinatoricsVector<String>> comb : gen) {
+            System.out.println(ComplexCombinationGenerator.convert2String(comb)
+                    + " - " + comb);
+        }
+    }
 
-		// Iterate the combinations
-		for (ICombinatoricsVector<ICombinatoricsVector<String>> comb : gen) {
-			System.out.println(ComplexCombinationGenerator.convert2String(comb)
-					+ " - " + comb);
-		}
-	}
+    static void simple_subsets() {
 
-	static void simpleSubSets() {
+        System.out.println("===== All subsets: =====");
 
-		System.out.println("simpleSubSets");
+        // Create an initial vector/set
+        ICombinatoricsVector<String> initialSet = Factory
+                .createVector(new String[]{"one", "two", "three"});
 
-		// Create an initial vector/set
-		ICombinatoricsVector<String> initialSet = Factory
-				.createVector(new String[] { "one", "two", "three" });
+        // Create an instance of the subset generator
+        Generator<String> gen = Factory.createSubSetGenerator(initialSet);
 
-		// Create an instance of the subset generator
-		Generator<String> gen = Factory.createSubSetGenerator(initialSet);
+        // Print the subsets
+        for (ICombinatoricsVector<String> subSet : gen) {
+            System.out.println(subSet);
+        }
+    }
 
-		// Print the subsets
-		for (ICombinatoricsVector<String> subSet : gen) {
-			System.out.println(subSet);
-		}
-	}
+    static void duplicate_subsets() {
 
-	static void duplicateSubSets() {
+        System.out.println("===== All subsets with duplicates: =====");
 
-		System.out.println("duplicateSubSets");
+        // Create an initial vector/set (a, b, a, c)
+        ICombinatoricsVector<String> initialSet = Factory
+                .createVector(new String[]{"a", "b", "a", "c"});
 
-		// Create an initial vector/set (a, b, a, c)
-		ICombinatoricsVector<String> initialSet = Factory
-				.createVector(new String[] { "a", "b", "a", "c" });
+        // Create an instance of the subset generator
+        Generator<String> gen = Factory
+                .createSubSetGenerator(initialSet, false);
 
-		// Create an instance of the subset generator
-		Generator<String> gen = Factory
-				.createSubSetGenerator(initialSet, false);
+        // Print the subsets
+        for (ICombinatoricsVector<String> subSet : gen) {
+            System.out.println(subSet);
+        }
+    }
 
-		// Print the subsets
-		for (ICombinatoricsVector<String> subSet : gen) {
-			System.out.println(subSet);
-		}
-	}
+    static void integer_partition() {
 
-	static void integerPartition() {
+        System.out.println("===== All integer partitions of 5: =====");
 
-		System.out.println("integerPartition");
+        // Create an instance of the partition generator to generate all
+        // possible partitions of 5
+        Generator<Integer> gen = Factory.createPartitionGenerator(5);
 
-		// Create an instance of the partition generator to generate all
-		// possible partitions of 5
-		Generator<Integer> gen = Factory.createPartitionGenerator(5);
+        // Print the partitions
+        for (ICombinatoricsVector<Integer> p : gen) {
+            System.out.println(p);
+        }
+    }
 
-		// Print the partitions
-		for (ICombinatoricsVector<Integer> p : gen) {
-			System.out.println(p);
-		}
-	}
+    static void integer_composition() {
 
-	static void integerComposition() {
+        System.out.println("===== All integer combinations of 5: =====");
 
-		System.out.println("integerComposition");
+        // Create an instance of the integer composition generator to generate
+        // all possible compositions of 5
+        Generator<Integer> gen = Factory.createCompositionGenerator(5);
 
-		// Create an instance of the integer composition generator to generate
-		// all
-		// possible compositions of 5
-		Generator<Integer> gen = Factory.createCompositionGenerator(5);
+        // Print the compositions
+        for (ICombinatoricsVector<Integer> p : gen) {
+            System.out.println(p);
+        }
+    }
 
-		// Print the compositions
-		for (ICombinatoricsVector<Integer> p : gen) {
-			System.out.println(p);
-		}
-	}
+    static void simple_with_equal_elements_permutation() {
 
-	static void simpleWithEqualElementsPermutation() {
+        System.out.println("===== Simple With Equal Elements Permutations: =====");
 
-		System.out.println("simpleWithEqualElementsPermutation");
+        // Create the initial vector
+        ICombinatoricsVector<Integer> initialVector = Factory
+                .createVector(new Integer[]{1, 1, 2, 2});
 
-		// Create the initial vector
-		ICombinatoricsVector<Integer> initialVector = Factory
-				.createVector(new Integer[] { 1, 1, 2, 2 });
+        // Create the generator
+        Generator<Integer> generator = Factory
+                .createPermutationGenerator(initialVector);
 
-		// Create the generator
-		Generator<Integer> generator = Factory
-				.createPermutationGenerator(initialVector);
+        for (ICombinatoricsVector<Integer> perm : generator) {
+            System.out.println(perm);
+        }
+    }
 
-		for (ICombinatoricsVector<Integer> perm : generator) {
-			System.out.println(perm);
-		}
-	}
+    static void simple_five_integer_composition_range() {
 
-	static void simpleFiveIntegerCompositionRange() {
+        System.out.println("===== Simple Five Integer Composition Range: =====");
 
-		System.out.println("simpleFiveIntegerCompositionRange");
+        IntegerGenerator compositionGenerator = IntegerFactory
+                .createIntegerCompositionGenerator(5);
 
-		IntegerGenerator compositionGenerator = IntegerFactory
-				.createIntegerCompositionGenerator(5);
+        List<IntegerVector> list = compositionGenerator.generateObjectsRange(5, 7);
 
-		List<IntegerVector> list = compositionGenerator.generateObjectsRange(5,
-				7);
+        for (IntegerVector vector : list) {
+            System.out.println(vector);
+        }
+    }
 
-		for (IntegerVector vector : list) {
-			System.out.println(vector);
-		}
-	}
+    /**
+     * Print all 3-combinations of the set (apple, orange, cherry, melon) which
+     * contains orange
+     */
+    static void filtered_combinations() {
 
-	/**
-	 * Print all 3-combinations of the set (apple, orange, cherry, melon) which
-	 * contains orange
-	 */
-	static void filteredCombinations() {
+        System.out.println("===== Filtered Combinations: =====");
 
-		System.out.println("filteredCombinations");
+        // Create the initial set/vector of 3 elements (apple, orange, cherry,
+        // melon)
+        ICombinatoricsVector<String> originalVector = Factory
+                .createVector(new String[]{"apple", "orange", "cherry",
+                        "melon"});
 
-		// Create the initial set/vector of 3 elements (apple, orange, cherry,
-		// melon)
-		ICombinatoricsVector<String> originalVector = Factory
-				.createVector(new String[] { "apple", "orange", "cherry",
-						"melon" });
+        // Create the combination generator by calling the appropriate method in
+        // the Factory class
+        Generator<String> gen = Factory.createSimpleCombinationGenerator(
+                originalVector, 3);
 
-		// Create the combination generator by calling the appropriate method in
-		// the Factory class
-		Generator<String> gen = Factory.createSimpleCombinationGenerator(
-				originalVector, 3);
+        // Create a filter and generate the results
+        List<ICombinatoricsVector<String>> result = gen
+                .generateFilteredObjects(new IFilter<ICombinatoricsVector<String>>() {
 
-		// Create a filter and generate the results
-		List<ICombinatoricsVector<String>> result = gen
-				.generateFilteredObjects(new IFilter<ICombinatoricsVector<String>>() {
+                    // returns true if the value is accepted
+                    public boolean accepted(long index,
+                                            ICombinatoricsVector<String> value) {
+                        return value.contains("orange");
+                    }
 
-					// returns true if the value is accepted
-					public boolean accepted(long index,
-							ICombinatoricsVector<String> value) {
-						return value.contains("orange");
-					}
+                });
 
-				});
+        // Print the result
+        for (ICombinatoricsVector<String> perm : result)
+            System.out.println(perm);
 
-		// Print the result
-		for (ICombinatoricsVector<String> perm : result)
-			System.out.println(perm);
-
-	}
+    }
 
 }
